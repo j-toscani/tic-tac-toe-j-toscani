85fa8ba592f6d6a11a39120d46600db47d7ead7c
j-toscani <julian_tosani@gmx.de>
2019-09-10 11:49:02 +0200

    tested properties on components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Square from "./Square";
 
+function renderSquare(index) {
+  return <Square abc={index} />;
+}
 export default function Board() {
   const status = "Next player: X";
 
@@ -8,19 +11,19 @@ export default function Board() {
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(7)}
+        {renderSquare(8)}
+        {renderSquare(9)}
       </div>
     </div>
   );
